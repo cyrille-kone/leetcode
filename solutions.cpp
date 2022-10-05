bfe86ce85e67fa51a10c9ebfdb6051506521a481
cyrille-kone <99673185+cyrille-kone@users.noreply.github.com>
2022-10-05 12:23:40 +0200

    Update solutions.cpp

--- solutions.cpp
+++ solutions.cpp
@@ -1,6 +1,10 @@
-// 830. Positions of Large Groups
+#include<vector>
+#include<string>
+#include<iostream>
+using namespace std;
 class Solution {
 public:
+    /* 830. Positions of Large Groups*/
     vector<vector<int>> largeGroupPositions(string s) {
         vector<vector<int>> res;
         size_t  k{s.length()};
@@ -15,4 +19,27 @@ public:
         if ( start <= k-3) res.push_back({start, i-1});
         return res;
     }
+    
+    /* 2243. Calculate Digit Sum of a String */
+    string digitSum(string s, size_t k) {
+        string res{s};
+        std::stringstream ss;
+        int total, add;
+        while(res.length() > k){
+            for(size_t i=0; i < res.length(); i+=k){
+                add = min(k, res.length() - i);
+                total = 0;
+                for(size_t j=i; j<i+add;j++){
+                    total += res[j] - '0';
+                }
+                ss<<total;
+            }
+            res = ss.str();
+            ss.str(string());
+        }
+        return res;
+    }
 };
+
+
+
